887704aa767c1ba5a118fa113ee30cb2857f8633
deborapavani <de.pavani@icloud.com>
2021-09-02 13:08:48 -0300

    working

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import "./App.css";
+import { useState } from "react";
+
+export default function App() {
+  const [fruits, setFruits] = useState([
+    { name: "banana", color: "yellow", price: 2 },
+    { name: "cherry", color: "red", price: 3 },
+    { name: "strawberry", color: "red", price: 4 },
+  ]);
+
+  const maped = fruits.map((element) => element.name);
+  console.log(maped);
+  const reduced = fruits.reduce((acc, fruit) => acc + fruit.price, 0);
+
+  const filterRedFruits = () => {
+    const filtered = fruits.filter((fruit) => fruit.color === "red");
+    setFruits(filtered);
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div> Preço total: = {reduced}</div>
+        <div>
+          <li>{maped}</li>
+        </div>
+        <button onClick={filterRedFruits}>Mostrar frutas vermelhas</button>
+      </header>
+    </div>
+  );
+}
